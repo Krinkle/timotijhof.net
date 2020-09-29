f3851a9212081428850b13b096a3fd4933cead3f
Timo Tijhof <krinklemail@gmail.com>
2020-09-29 03:41:51 +0100

    theme: avoid inline tooltips for in-page links
    Only display the mobile-friendly "(i)" button next to external
    links, not e.g. for the footnote backlinks.

--- _plugins/anchor_tooltip.rb
+++ _plugins/anchor_tooltip.rb
@@ -2,14 +2,20 @@
 #
 # Transform anchor tags with tooltips (see plainwhite.scss).
 #
+# If used in cunjuction with ExternalLinkFilter, then AnchorTooltipFilter
+# must be applied first.
+#
+#
 # Example:
 #
 #  {{ post.content | fix_tooltip }}
 #
+#  {{ post.content | fix_tooltip | fix_extlink }}
+#
 module Jekyll
 	module AnchorTooltipFilter
 		def fix_tooltip(input)
-			input.gsub /(<a [^>]+? (title="[^>"]+?")>[^<]+?)<\/a>/, '\1<span \2 tabindex="0"></span></a>'
+			input.gsub /(<a href="[^#][^>"]+" (title="[^>"]+?")>[^<]+?)<\/a>/, '\1<span \2 tabindex="0"></span></a>'
 		end
 	end
 end
